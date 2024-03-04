9e852290bd4a20b07de35b8b6f5465289bf701e5
ursgarsan <ursgarsan@alum.us.es>
2024-03-04 17:11:06 +0100

    intento de hbs

--- web_app/app.js
+++ web_app/app.js
@@ -1,31 +1,43 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { engine } =require('express-handlebars');
+const path = require('path');
+
 const profesoresRoutes = require('./routes/profesorRoutes');
 const asignaturasRoutes = require('./routes/asignaturaRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const ejs = require('ejs');
-
-app.set('view engine', 'ejs');
 
 mongoose.connect('mongodb://127.0.0.1:27017/PAP')
   .then(() => console.log('Conexión exitosa a MongoDB'))
   .catch(err => console.error('Error al conectar a MongoDB:', err));
 
-app.use(express.json());
+// Configurar Handlebars como motor de plantillas
+app.engine('handlebars', engine({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  defaultLayout: 'main',
+}));
+app.set('view engine', '.hbs');
 
-// Ruta para la página de inicio
+// Definir la ruta para la página de inicio
 app.get('/', (req, res) => {
-  res.render('pagina', { title: 'Título de la página', content: 'Contenido de la página de inicio' });
+  res.render('index', { title: 'Página de inicio' });
 });
+
+app.use(express.json());
 
 // Rutas para otras funcionalidades
 app.use('/profesores', profesoresRoutes);
 app.use('/asignaturas', asignaturasRoutes);
 
+// Establecer el directorio de vistas
+app.set('views', path.join(__dirname, 'views'));
+
 app.listen(PORT, () => {
-  console.log(`Servidor iniciado en el puerto ${PORT}`);
+  console.log(`Servidor iniciado en http://localhost:${PORT}`);
 });
 
 module.exports = app;
