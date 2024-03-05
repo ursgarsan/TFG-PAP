af6a3758cd7ebdecb1c3a8b9e980050ec999c66d
ursgarsan <ursgarsan@alum.us.es>
2024-03-05 16:01:11 +0100

    layout parametrizado y barra corregida

--- web_app/controllers/asignaturaController.js
+++ web_app/controllers/asignaturaController.js
@@ -3,7 +3,8 @@ const Asignatura = require('../models/asignaturaModel');
 exports.getAllAsignaturas = async (req, res) => {
     try {
       const asignaturas = await Asignatura.find();
-      res.render('asignaturas', { asignaturas });
+      const title = 'Asignaturas';
+      res.render('asignaturas', { asignaturas, title});
     } catch (error) {
       console.error('Error al obtener asignaturas:', error);
       res.status(500).json({ message: 'Error interno del servidor' });
--- web_app/controllers/profesorController.js
+++ web_app/controllers/profesorController.js
@@ -3,7 +3,8 @@ const Profesor = require('../models/profesorModel');
 exports.getAllProfesores = async (req, res) => {
     try {
       const profesores = await Profesor.find();
-      res.render('profesores', { profesores });
+      const title = 'Profesores';
+      res.render('profesores', { profesores, title });
     } catch (error) {
       console.error('Error al obtener profesores:', error);
       res.status(500).json({ message: 'Error interno del servidor' });
