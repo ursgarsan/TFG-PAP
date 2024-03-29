ced09e771ea42fbe63fb2937f0980a1e5e2b6975
ursgarsan <ursgarsan@alum.us.es>
2024-03-29 13:49:45 +0100

    formateo horas

--- poblar_bbdd.py
+++ poblar_bbdd.py
@@ -2,13 +2,31 @@ import pandas as pd
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
-db = client['PAP']
+db_name = 'PAP'
+
+if db_name in client.list_database_names():
+    client.drop_database(db_name)
+
+db = client[db_name]
+
+admin_colecc = db['administradores']
 prof_colecc = db['profesores']
 asign_colecc = db['asignaturas']
 grupo_colecc = db['grupos']
 
 datos_prof = pd.read_excel("docs_iniciales/infoProf.xlsx", usecols=[0, 1, 2], header=None)
 datos_dep = pd.read_excel("docs_iniciales/infoDep.xlsx", header=None)
+
+admin = {
+    'nombre': 'Administrador',
+    'apellidos': 'Administrador',
+    'usuario': 'admin',
+    'pass': 'admin'
+}
+
+admin_colecc.insert_one(admin)
+
+print("Administrador creado e insertado correctamente.")
 
 capacidades = datos_dep.iloc[1, 11:]
 for indice,fila in datos_prof.iterrows():
@@ -45,7 +63,36 @@ for indice, fila in asignaturas.iterrows():
 
 print("Datos insertados en la colección 'asignaturas' de la base de datos 'PAP'.")
 
-# Ahora actualizamos el campo 'grupos' de las asignaturas
+def formatHorario(horario):
+    new_horario = []
+    for elemento in horario:
+        clases_separadas = elemento.split(' // ')
+        for clase in clases_separadas:
+            if '-' in clase:
+                dia, hora = clase.split(' - ')
+            else:
+                dia = clase
+                hora = None
+            if hora is None:
+                hora = '18:30 a 19:50'
+            else:
+                # si no existe hora de fin se le pone la predeterminada con su duración redeterminada (1h y 50min)
+                if 'a' not in hora:
+                    horas_aux, minutos_aux = hora.split(':')
+                    horas_aux = int(horas_aux)
+                    minutos_aux = int(minutos_aux)
+                    horas_aux += 1
+                    minutos_aux += 50
+                    if minutos_aux >= 60:
+                        minutos_aux -= 60
+                        horas_aux +=1
+                    hora_aux = f"{horas_aux}:{minutos_aux:02d}"
+                    hora = hora + ' a ' + hora_aux
+            dias = dia.replace('.', '').split(' ')
+            new_horario.append({'dias': dias, 'hora': hora})
+
+    return new_horario
+
 for indice, fila in asignaturas.iterrows():
     horario = []
     if not pd.isnull(fila[9]):
@@ -62,11 +109,11 @@ for indice, fila in asignaturas.iterrows():
         'tipo': tipo,
         'grupo': fila[5],
         'cuatrimestre': fila[6],
-        'acreditacion': fila[7]
+        'acreditacion': fila[7],
     }
 
     if horario:
-        grupo['horario'] = horario
+        grupo['horario'] = formatHorario(horario)
 
     # Buscar la asignatura correspondiente utilizando el acrónimo
     asignatura_correspondiente = asign_colecc.find_one({'codigo': fila[1]})
