931a63c121f5ce7b179919f88285c3b7a93adf94
ursgarsan <ursgarsan@alum.us.es>
2024-03-30 18:28:58 +0100

    creacion de modelos de peticion y asignacion

---
+++ web_app/models/asignacionModel.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+const asignacionSchema = new Schema({
+    docente: {
+        type: Schema.Types.ObjectId,
+        ref: 'Profesor',
+        require: true,
+    },
+    grupo: {
+        type: Schema.Types.ObjectId,
+        ref: 'Grupo',
+        require: true,
+    },
+    curso: {
+        type: String,
+        require: true,
+    }
+})
+
+const Asignacion = model('Asignacion', asignacionSchema);
+module.exports = Asignacion;
---
+++ web_app/models/peticionModel.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const peticionSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'Profesor',
+        require: true,
+    },
+    grupo: {
+        type: Schema.Types.ObjectId,
+        ref: 'Grupo',
+        require: true,
+    },
+    orden: {
+        type: Number,
+        require: true,
+    },
+    status: {
+        type: String,
+        require: true,
+        default: 'solicitado',
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    curso: {
+        type: String,
+        require: true,
+    }
+})
+const Peticion = model('Peticion', peticionSchema);
+module.exports = Peticion;
