9d4a6f66d42ca21ecf244da7cd15a82004e27077
ursgarsan <ursgarsan@alum.us.es>
2024-03-30 19:49:23 +0100

    crear profesor

--- web_app/controllers/profesorController.js
+++ web_app/controllers/profesorController.js
@@ -2,12 +2,33 @@ const Profesor = require('../models/profesorModel');
 const { esAdmin } = require('../utils/authUtils');
 
 exports.getAllProfesores = async (req, res) => {
-    try {
-      const profesores = await Profesor.find();
-      const title = 'Profesores';
-      res.render('profesores', { profesores, title });
-    } catch (error) {
-      console.error('Error al obtener profesores:', error);
-      res.status(500).json({ message: 'Error interno del servidor' });
-    }
-  };
+  try {
+    const profesores = await Profesor.find();
+    const title = 'Profesores';
+    res.render('profesores', { profesores, title });
+  } catch (error) {
+    console.error('Error al obtener profesores:', error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+};
+
+exports.createForm = async (req, res) => {
+  try {
+    res.render('createProfesor', { title: 'Agregar Nuevo Profesor' });
+  } catch (error) {
+    console.error('Error al renderizar formulario:', error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+};
+
+exports.createProfesor = async (req, res) => {
+  try {
+    const { orden, nombre, uvus, capacidad } = req.body;
+    const nuevoProfesor = new Profesor({ orden, nombre, uvus, capacidad });
+    await nuevoProfesor.save();
+    res.redirect('/profesores'); 
+  } catch (error) {
+    console.error('Error al crear profesor:', error);
+    res.status(500).json({ message: 'Error interno del servidor' });
+  }
+};
--- web_app/routes/profesorRoutes.js
+++ web_app/routes/profesorRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const profesorController = require('../controllers/profesorController');
 
 router.get('/', profesorController.getAllProfesores);
+router.get('/create', profesorController.createForm);
+router.post('/create', profesorController.createProfesor);
 
 module.exports = router;
