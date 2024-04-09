650ae85e2f44ddd6fb6bd7a48ec4e20957e628c6
ursgarsan <ursgarsan@alum.us.es>
2024-04-09 16:55:30 +0200

    cambio de nombre

--- routes/grupoRoutes.js
+++ routes/grupoRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const grupoController = require('../controllers/grupoController');
 const { requireAdmin } = require('../utils/authUtils');
-const { createGrupoValidation } = require('../validations/groupValidation');
+const { createGrupoValidation } = require('../validations/grupoValidation');
 
 router.get('/create', requireAdmin, grupoController.createForm);
 router.post('/create', requireAdmin, createGrupoValidation, grupoController.createGrupo);
