0624a1dd062d30d73d2c9ef325ec56c77d6c42be
ursgarsan <ursgarsan@alum.us.es>
2024-04-16 13:00:31 +0200

    añadido poblado para pruebas de bbdd

---
+++ database/populate_db_demo.py
@@ -0,0 +1,147 @@
+import pandas as pd
+import bcrypt
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db_name = 'PAP'
+
+if db_name in client.list_database_names():
+    client.drop_database(db_name)
+
+db = client[db_name]
+
+admin_colecc = db['administradores']
+prof_colecc = db['profesores']
+asign_colecc = db['asignaturas']
+grupo_colecc = db['grupos']
+
+datos_prof = pd.read_excel("database/data/infoProfDemo.xlsx", usecols=[0, 1, 2, 3], header=None)
+datos_dep = pd.read_excel("database/data/infoDepDemo.xlsx", header=None)
+
+def generar_hash_contraseña(contraseña_plana):
+    return bcrypt.hashpw(contraseña_plana.encode('utf-8'), bcrypt.gensalt())
+
+admin = {
+    'nombre': 'Administrador',
+    'apellidos': 'Administrador',
+    'usuario': 'admin',
+    'pass': generar_hash_contraseña('admin')
+}
+
+admin_colecc.insert_one(admin)
+
+print("Administrador creado e insertado correctamente.")
+
+def formatNombre(nombre_original):
+    apellidos, nombre = nombre_original.split(', ')
+    return nombre, apellidos
+
+capacidades = datos_dep.iloc[1, 11:]
+for indice,fila in datos_prof.iterrows():
+    nombre,apellidos = formatNombre(fila[3])
+    profesor = {
+        'orden': fila[0],
+        'nombre': nombre,
+        'apellidos': apellidos,
+        'uvus': fila[2],
+        'capacidad': capacidades[indice + 11]
+    }
+    prof_colecc.insert_one(profesor)
+
+print("Datos insertados en 'profesores' de la base de datos 'PAP'.")
+
+asignaturas = datos_dep.iloc[7:, :11]
+
+for indice, fila in asignaturas.iterrows():
+    asignatura = {
+        'nombre': fila[2],
+        'titulacion': fila[0],
+        'codigo': str(fila[1]),
+        'acronimo': fila[3],
+        'curso': '2023-2024',
+        'grupos': []
+    }
+
+    asignatura_existente = asign_colecc.find_one({
+        'nombre': asignatura['nombre'],
+        'titulacion': asignatura['titulacion'],
+        'codigo': str(asignatura['codigo']),
+        'acronimo': asignatura['acronimo']
+    })
+
+    if asignatura_existente is None:
+        asign_colecc.insert_one(asignatura)
+
+print("Datos insertados en 'asignaturas' de la base de datos 'PAP'.")
+
+def formatHorario(horario):
+    new_horario = []
+    for elemento in horario:
+        clases_separadas = elemento.split(' // ')
+        for clase in clases_separadas:
+            if '-' in clase:
+                dia, hora = clase.split(' - ')
+            else:
+                dia = clase
+                hora = None
+            if hora is None:
+                hora_inicio = '18:30'
+                hora_fin = '19:50'
+            else:
+                # si no existe hora de fin se le pone la predeterminada con su duración redeterminada (1h y 50min)
+                if 'a' not in hora:
+                    hora_inicio = hora
+                    horas_aux, minutos_aux = hora.split(':')
+                    horas_aux = int(horas_aux)
+                    minutos_aux = int(minutos_aux)
+                    horas_aux += 1
+                    minutos_aux += 50
+                    if minutos_aux >= 60:
+                        minutos_aux -= 60
+                        horas_aux +=1
+                    hora_aux = f"{horas_aux}:{minutos_aux:02d}"
+                    hora_fin = hora_aux
+                else:
+                    hora_inicio, hora_fin = hora.split(' a ')
+            dias = dia.split(' ')
+            new_horario.append({'dias': dias, 'hora_inicio': hora_inicio, 'hora_fin': hora_fin})
+
+    return new_horario
+
+for indice, fila in asignaturas.iterrows():
+    horario = []
+    if not pd.isnull(fila[9]):
+        horario.append(fila[9])
+    if not pd.isnull(fila[10]):
+        horario.append(fila[10])
+
+    if fila[4] in ['Turno 1', 'Turno 2']:
+        tipo = fila[4]
+    else:
+        tipo = 'Teoría' if fila[4] in ['A', 'B'] else 'Laboratorio'
+    
+    grupo = {
+        'tipo': tipo,
+        'grupo': fila[5],
+        'cuatrimestre': fila[6],
+        'acreditacion': fila[7],
+        'curso': '2023-2024'
+    }
+
+    if horario:
+        grupo['horario'] = formatHorario(horario)
+
+    # Buscar la asignatura correspondiente utilizando el acrónimo
+    asignatura_correspondiente = asign_colecc.find_one({'codigo': str(fila[1])})
+    if asignatura_correspondiente:
+        grupo['asignatura_id'] = asignatura_correspondiente['_id']
+        grupo_colecc.insert_one(grupo)
+        # Actualizar la lista de grupos de la asignatura
+        asign_colecc.update_one(
+            {'_id': asignatura_correspondiente['_id']},
+            {'$push': {'grupos': grupo['_id']}}
+        )
+    else:
+        print(f"No se pudo encontrar la asignatura correspondiente al codigo '{fila[1]}' para el grupo '{fila[5]}'")
+
+print("Datos insertados en 'grupos' de la base de datos 'PAP'.")
---
+++ database/populate_demo.js
@@ -0,0 +1,22 @@
+const { exec } = require('child_process');
+const path = require('path');
+const scriptPath = path.join(__dirname, 'populate_db_demo.py');
+
+// Ejecutar el script Python para la inicialización de la base de datos Mongo
+const child = exec(`python ${scriptPath}`);
+
+child.stdout.on('data', (data) => {
+    console.log(`Salida del script de inicialización de la base de datos:\n${data}`);
+});
+
+child.stderr.on('data', (data) => {
+    console.error(`Error al ejecutar el script de inicialización de la base de datos:\n${data}`);
+});
+
+child.on('close', (code) => {
+    if (code === 0) {
+        console.log('Script de inicialización de la base de datos ejecutado exitosamente.');
+    } else {
+        console.error(`El script de inicialización de la base de datos se cerró con el código de salida ${code}.`);
+    }
+});
