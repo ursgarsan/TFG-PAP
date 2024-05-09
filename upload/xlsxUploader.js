746647c522146870f2cc61e3647d8acac85840ea
ursgarsan <ursgarsan@alum.us.es>
2024-05-09 10:33:55 +0200

    correccion dashboard

--- upload/xlsxUploader.js
+++ upload/xlsxUploader.js
@@ -15,12 +15,12 @@ router.post('/upload', upload.single('xlsxFile'), async (req, res) => {
   try {
     const file = req.file;
     if (!file) {
-      return res.redirect(await addQueryParams('/dashboard', { uploaded: false, error: 'No se ha proporcionado ningún archivo' }));
+      return res.redirect(await addQueryParams('/', { uploaded: false, error: 'No se ha proporcionado ningún archivo' }));
     }
 
     const fileExtension = path.extname(file.originalname);
     if (fileExtension.toLowerCase() !== '.xlsx') {
-      return res.redirect(await addQueryParams('/dashboard', { uploaded: false, error: 'El archivo no es de tipo .xlsx' }));
+      return res.redirect(await addQueryParams('/', { uploaded: false, error: 'El archivo no es de tipo .xlsx' }));
     }
 
     try {
@@ -33,7 +33,7 @@ router.post('/upload', upload.single('xlsxFile'), async (req, res) => {
   
     } catch (error) {
       console.error('Error al procesar el archivo XLSX:', error);
-      res.redirect(await addQueryParams('/dashboard', { uploaded: false, error: 'Error al eliminar los datos anteriores' }));
+      res.redirect(await addQueryParams('/', { uploaded: false, error: 'Error al eliminar los datos anteriores' }));
     }
 
     const workbook = XLSX.read(file.buffer, { type: 'buffer' });
@@ -62,7 +62,7 @@ router.post('/upload', upload.single('xlsxFile'), async (req, res) => {
     
         await profesor.save();
       } catch (error) {
-        return res.redirect(await addQueryParams('/dashboard', { uploaded: false, error: `Error al guardar el profesor de la columna ${i + 1}: ${error.message}` }));
+        return res.redirect(await addQueryParams('/', { uploaded: false, error: `Error al guardar el profesor de la columna ${i + 1}: ${error.message}` }));
       }
     }
 
@@ -85,7 +85,7 @@ router.post('/upload', upload.single('xlsxFile'), async (req, res) => {
           await asignatura.save();
         }        
       } catch (error){
-        return res.redirect(await addQueryParams('/dashboard', { uploaded: false, error: `Error al guardar la asignatura de la fila ${i + 1}: ${error.message}` }));
+        return res.redirect(await addQueryParams('/', { uploaded: false, error: `Error al guardar la asignatura de la fila ${i + 1}: ${error.message}` }));
       }
 
 
@@ -137,12 +137,12 @@ router.post('/upload', upload.single('xlsxFile'), async (req, res) => {
         try {
             await new Grupo(grupo).save();
         } catch (error) {
-            return res.redirect(await addQueryParams('/dashboard', { uploaded: false, error: `Error al guardar el grupo: ${JSON.stringify(grupo)}` }));
+            return res.redirect(await addQueryParams('/', { uploaded: false, error: `Error al guardar el grupo: ${JSON.stringify(grupo)}` }));
 
         }
     }
     } catch (error) {
-      res.redirect(await addQueryParams('/dashboard', { uploaded: false, error: 'Error al guardar los grupos en la base de datos, asegúrese de que el formato es el acordado' }));
+      res.redirect(await addQueryParams('/', { uploaded: false, error: 'Error al guardar los grupos en la base de datos, asegúrese de que el formato es el acordado' }));
     }
 
     const peticionesRange = { s: { c: 8, r: 7 }, e: { c: fullRange.e.c, r: fullRange.e.r } };
@@ -203,9 +203,9 @@ router.post('/upload', upload.single('xlsxFile'), async (req, res) => {
         }
       }
     }
-    res.redirect(await addQueryParams('/dashboard', { uploaded: true}));
+    res.redirect(await addQueryParams('/', { uploaded: true}));
   } catch (error) {
-    res.redirect(await addQueryParams('/dashboard', { uploaded: false, error: 'Error al procesar el archivo' }));
+    res.redirect(await addQueryParams('/', { uploaded: false, error: 'Error al procesar el archivo' }));
   }
 });
 
