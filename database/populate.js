6bed9a38aac4edf325763ae223f9ff86d0cb953b
ursgarsan <ursgarsan@alum.us.es>
2024-05-09 10:41:11 +0200

    migracion de py a js

--- database/populate.js
+++ database/populate.js
@@ -1,22 +1,42 @@
-const { exec } = require('child_process');
-const path = require('path');
-const scriptPath = path.join(__dirname, 'populate_db.py');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-// Ejecutar el script Python para la inicialización de la base de datos Mongo
-const child = exec(`python ${scriptPath}`);
+const admin_colecc = 'administradores';
 
-child.stdout.on('data', (data) => {
-    console.log(`Salida del script de inicialización de la base de datos:\n${data}`);
-});
+mongoose.connect('mongodb://127.0.0.1:27017/PAP')
+  .then(() => {
+    console.log('Conexión exitosa a MongoDB');
+    mongoose.connection.db.dropDatabase()
+      .then(() => {
+        console.log('Base de datos borrada exitosamente');
+        const Admin = mongoose.model(admin_colecc, new mongoose.Schema({
+          nombre: String,
+          apellidos: String,
+          usuario: String,
+          pass: String
+        }));
 
-child.stderr.on('data', (data) => {
-    console.error(`Error al ejecutar el script de inicialización de la base de datos:\n${data}`);
-});
+        bcrypt.hash('admin', 10, (err, hash) => {
+          if (err) {
+            console.error('Error al generar el hash de la contraseña:', err);
+            process.exit(1);
+          }
 
-child.on('close', (code) => {
-    if (code === 0) {
-        console.log('Script de inicialización de la base de datos ejecutado exitosamente.');
-    } else {
-        console.error(`El script de inicialización de la base de datos se cerró con el código de salida ${code}.`);
-    }
-});
+          const admin = new Admin({
+            nombre: 'Administrador',
+            apellidos: 'Administrador',
+            usuario: 'admin',
+            pass: hash
+          });
+
+          admin.save()
+            .then(() => {
+              console.log('Administrador creado e insertado correctamente.');
+              mongoose.connection.close().then(() => console.log('Conexión a MongoDB cerrada.'));
+            })
+            .catch(err => console.error('Error al insertar el administrador:', err));
+        });
+      })
+      .catch(err => console.error('Error al borrar la base de datos:', err));
+  })
+  .catch(err => console.error('Error al conectar a MongoDB:', err));
