01d44004de077acd008ec89e637ed375b1bd695d
ursgarsan <ursgarsan@alum.us.es>
2024-05-14 10:11:51 +0200

    completada validación de asignaturas

--- validations/asignaturaValidation.js
+++ validations/asignaturaValidation.js
@@ -1,5 +1,5 @@
 const { body } = require('express-validator');
 
 exports.createAsignaturaValidation = [
-  body('acronimo').notEmpty().withMessage('El acrónimo es requerido')
+  body('acronimo').notEmpty().withMessage('El acrónimo es requerido').matches(/[a-zA-Z]/).withMessage('El acrónimo no puede ser un valor únicamente numérico')
 ];
