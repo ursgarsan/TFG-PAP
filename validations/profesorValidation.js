80972b687c0d72ab69a3178d9da2787a3539276b
ursgarsan <ursgarsan@alum.us.es>
2024-05-14 12:29:31 +0200

    validacion profesores

--- validations/profesorValidation.js
+++ validations/profesorValidation.js
@@ -1,9 +1,9 @@
 const { body } = require('express-validator');
 
 exports.createProfesorValidation = [
-  body('orden').notEmpty().withMessage('El acrónimo es requerido'),
-  body('nombre').notEmpty().withMessage('El acrónimo es requerido'), 
-  body('apellidos').notEmpty().withMessage('El acrónimo es requerido'),
-  body('uvus').notEmpty().withMessage('El acrónimo es requerido'),
-  body('capacidad').notEmpty().withMessage('El acrónimo es requerido'),       
+  body('orden').notEmpty().withMessage('El orden es requerido'),
+  body('nombre').notEmpty().withMessage('El nombre es requerido'), 
+  body('apellidos').notEmpty().withMessage('Los apellidos son requeridos'),
+  body('uvus').notEmpty().withMessage('El uvus es requerido'),
+  body('capacidad').notEmpty().withMessage('La capacidad acreditativa es requerida'),       
 ];
